8b4019911d4a4c12b4148bfbd1cf339ceb452a06
Washingto Pires <4washingtonbr@gmail.com>
2019-11-20 15:38:16 -0300

    Installing "react-native-paper" lib.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import {
   Text,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const App = () => {
   return (
-    <>
+    <PaperProvider>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Icon name="rocket" size={30} color="#900" />
         <Text>Hello World!</Text>
       </SafeAreaView>
-    </>
+    </PaperProvider>
   );
 };
 
