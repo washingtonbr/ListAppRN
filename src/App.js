040b92190214c080f44cbb82d5f380d5ed4b522a
Washingto Pires <4washingtonbr@gmail.com>
2019-11-20 14:39:32 -0300

    Installing "react-native-vector-icons" lib.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {
   SafeAreaView,
   Text,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
+        <Icon name="rocket" size={30} color="#900" />
         <Text>Hello World!</Text>
       </SafeAreaView>
     </>
