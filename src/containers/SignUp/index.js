e2131ebdd8b875728d359365bea70321688833b7
Washingto Pires <4washingtonbr@gmail.com>
2019-11-21 18:40:54 -0300

    Creating the Sign Up Container Component.

---
+++ src/containers/SignUp/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Title, TextInput, Button, Colors } from 'react-native-paper';
+
+export default function SignUpContainer({
+  email,
+  setEmail,
+  password,
+  setPassword,
+  signUp,
+}) {
+  return (
+    <>
+    <Title>Create your account</Title>
+
+    <TextInput
+        style={styles.formInput}
+        mode="outlined"
+        label='Email'
+        value={email}
+        onChangeText={val => setEmail(val)}
+    />
+
+    <TextInput
+        style={styles.formInput}
+        mode="outlined"
+        label='Password'
+        value={password}
+        onChangeText={val => setPassword(val)}
+    />
+
+    <Button
+        style={styles.formButton}
+        mode="contained"
+        onPress={signUp}>
+        Sign Up
+    </Button>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  formInput: {
+    backgroundColor: Colors.white,
+    marginVertical: 8,
+  },
+  formButton: {
+    marginVertical: 8,
+  },
+});
