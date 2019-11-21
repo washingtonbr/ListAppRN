2c885545be4975167fc8716d889d62f74a3e967b
Washingto Pires <4washingtonbr@gmail.com>
2019-11-21 18:43:08 -0300

    Using Sign Up and Sign In Containers on Auth Screen.

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Button } from 'react-native-paper';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Colors } from 'react-native-paper';
 import Header from '../../components/Header'
+import SignInContainer from '../../containers/SignIn';
+import SignUpContainer from '../../containers/SignUp';
 
 export default function AuthScreen({
   navigation
 }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function signIn() {
+    navigation.navigate('Panel');
+  }
+
+  function signUp() {
+    navigation.navigate('Panel');
+  }
+
   return (
     <>
       <Header />
-      <Text>Auth</Text>
-      <Button mode="contained" onPress={() => navigation.navigate('Panel')}>
-        Go to Panel
-      </Button>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.content}
+      >
+        <SignInContainer
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          signIn={signIn}
+        />
+
+        <SignUpContainer
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          signUp={signUp}
+        />
+      </ScrollView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.white,
+    flex: 1,
+  },
+  content: {
+    paddingVertical: 18,
+    paddingHorizontal: 10,
+  },
+});
